dec9909da9a9940226a82b7438a15a64a5496042
Brian Zuleta Tobón <89109378+BrianZuto@users.noreply.github.com>
2025-10-15 21:16:15 -0500

    Agregar ConfigService a los providers de la aplicaciÃ³n

--- inventpro-frontend/src/app/app.config.ts
+++ inventpro-frontend/src/app/app.config.ts
@@ -5,6 +5,7 @@ import { provideAnimations } from '@angular/platform-browser/animations';
 
 import { routes } from './app.routes';
 import { authInterceptor } from './interceptors/auth-interceptor';
+import { ConfigService } from './services/config.service';
 
 export const appConfig: ApplicationConfig = {
   providers: [
@@ -12,6 +13,7 @@ export const appConfig: ApplicationConfig = {
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes),
     provideHttpClient(withInterceptors([authInterceptor])),
-    provideAnimations()
+    provideAnimations(),
+    ConfigService
   ]
 };
